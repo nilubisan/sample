767f9beb18fda5055967790af6b6e8389b40018c
Rinat N <rinat-n@auto.kz>
2023-05-15 23:32:33 +0600

    dev: add firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDBq1GMw1aSDmE68xasrMTeCP5M0S_Ye2A",
+    authDomain: "nilubisan-app.firebaseapp.com",
+    projectId: "nilubisan-app",
+    storageBucket: "nilubisan-app.appspot.com",
+    messagingSenderId: "1039610879719",
+    appId: "1:1039610879719:web:5a818f79dd203fea844c42",
+    measurementId: "G-67CVXX5Y7K"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+
   return (
     <div className="App">
       <header className="App-header">
